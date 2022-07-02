3d7f8dcd21305ddd3649f59a1209eed46da2e309
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-07-02 09:02:25 +0530

    basics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,34 @@ import React from 'react';
 import './App.css';
 
 
+let name: string;
+let age: number|string;
+let isStudent: any;
+let hobbies: string[];
+let role: [number, string];
 
+
+
+// isStudent="ds"
+// role=[5,5];
+// role=[5,"aayush"];
+
+type Person={
+  name:string;
+  age?:number;
+}
+
+let person:Person={
+  name:"Aayush",
+  age:56
+}
+
+age="dsds"
+
+function printName(name:string){
+  console.log(name)
+}
+printName("Hello")
 function App() {
   return (
     <div className="App">
